0a6412a1e0403742837ebd82434477ca9c49ddb1
Nicolas Cailloux <caillouxnicolas@gmail.com>
2014-07-08 21:23:07 +0200

    cleaning

--- samples/SpaceInvaders/scripts/SpaceInvaders.js
+++ samples/SpaceInvaders/scripts/SpaceInvaders.js
@@ -2,31 +2,4 @@ var start = function() {
     game.init(new Level());
 }
 
-/**
- * Singleton holding all the images. Prevents images from being loaded twice. 
- */
-/**
-var imageHolder = new function () {
-    var nbImages = 2;
-    // background image
-    this.background = new Image();
-    this.background.src = "assets/images/background.png";
-    this.background.onload = imageLoaded_aux;
-    // spritesheet
-    this.spritesheet = new Image();
-    this.spritesheet.src = "assets/images/spritesheet.png";
-    this.spritesheet.onload = imageLoaded_aux;
-    // load count
-    var nbLoaded = 0;
-    var imageLoaded_aux = function() {
-	imageLoaded();
-    };
-    function imageLoaded() {
-	nbLoaded++;
-	if (nbLoaded == nbImages) {
-	    window.init();
-	}
-    }
-};
-*/
 var textureManager = new TextureManager();
