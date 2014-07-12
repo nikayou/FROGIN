a631e34fb48808d4da4b8c4f3abd3a2b0cec629c
Nicolas Cailloux <caillouxnicolas@gmail.com>
2014-07-12 16:03:51 +0200

    double-inclusion guard

--- src/scripts/Core/Include.js
+++ src/scripts/Core/Include.js
@@ -1,19 +1,31 @@
-// TODO check double inclusion
-var include = function(filePath) {
-    var script = document.createElement('script');
-    script.setAttribute("type", "text/JavaScript");
-    script.setAttribute("language", "JavaScript");
-    script.src = filePath;
-    console.log('including '+filePath);
-    var scriptsNode = document.getElementById('scripts');
-    // if a "scripts" container exists, the script is added to it. 
-    // otherwise, it is added to "head"
-    var node = null;
-    if (scriptsNode){
-	node = scriptsNode[0];
-    } else {
-	node = document.getElementsByTagName('head')[0];
-    }
-    node.insertBefore(script, node.childNodes[0]);
 
-}
+var includedFiles = [];
+(function(){
+    var scripts = document.getElementsByTagName("script");
+    for (i in scripts) {
+	includedFiles[scripts[i].src] = true;
+    }
+})();
+
+var include = function(filePath) {
+    if (!includedFiles[filePath]) {
+	includedFiles[filePath] = true;
+	var script = document.createElement('script');
+	script.setAttribute("type", "text/JavaScript");
+	script.setAttribute("language", "JavaScript");
+	script.src = filePath;
+	var scriptsNode = document.getElementById('scripts');
+	// if a "scripts" container exists, the script is added to it. 
+	// otherwise, it is added to "head"
+	var node = null;
+	if (scriptsNode){
+	    node = scriptsNode[0];
+	} else {
+	    node = document.getElementsByTagName('head')[0];
+	}
+	node.insertBefore(script, node.childNodes[0]);
+    }else{
+	console.log('will not include "'+filePath+'" twice');
+    }
+};
+
