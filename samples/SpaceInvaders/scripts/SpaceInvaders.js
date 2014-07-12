c491e19b515e3865240bcf3e706d1c7ce685fd6a
Nicolas Cailloux <caillouxnicolas@gmail.com>
2014-07-12 16:08:15 +0200

    moved scripts to "includes" on general file

--- samples/SpaceInvaders/scripts/SpaceInvaders.js
+++ samples/SpaceInvaders/scripts/SpaceInvaders.js
@@ -1,3 +1,9 @@
+include("scripts/Background.js");
+include("scripts/Bullet.js");
+include("scripts/Player.js");
+include("scripts/Enemy.js");
+include("scripts/Level.js");
+
 var start = function() {
     game.init(new Level());
 }
